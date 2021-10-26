9c1b257013069ade63980221c685e399d5d79044
davidkaggulire <dkaggulire@gmail.com>
2021-10-26 12:22:14 +0300

    add more tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,23 +1,74 @@
 """ tests.py """
 import sys
 import os
-import unittest
-import argparse
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
+import unittest
+import argparse
+from unittest.mock import patch
+from argparse import ArgumentParser
 from confluent_kafka import Producer
 from confluent_kafka import Consumer, KafkaError, KafkaException
-from chatapp import parse_args, read_messages, send_messages
+from chatapp import parsed_args, read_messages, send_messages, get_input
 
 
+class TestChatApp(unittest.TestCase):
 
-class TestScrapping(unittest.TestCase):
-    def test_parser(self):
-        parser = parse_args()
-        
+    def test_empty_parsed_args(self):
+        with self.assertRaises(SystemExit):
+            parsed_args(sys.argv[1:])
 
+    
+    def test_normal_parser(self):
+        """testing parsing args to function"""
+        parser = parsed_args(['send', '--channel', 'chat', '--server', 'localhost:9092'])
+        self.assertIsInstance(parser, dict)
+
+    
+    @patch('chatapp.parsed_args')
+    def test_parser_send(self, test_patch):
+        """testing parsing command send"""
+        test_patch.return_value = {'command': 'send', 'channel': 'chat', 'server': 'localhost:9092', 'group': None, 'from': 'start'}
+        parser = parsed_args(['send', '--channel', 'chat', '--server', 'localhost:9092'])
+        self.assertIsInstance(parser, dict)
+        self.assertEqual(parser, test_patch.return_value)
+
+    
+    @patch('chatapp.parsed_args')
+    def test_parser_receive(self, test_patch):
+        """testing parsing command receive"""
+        test_patch.return_value = {'command': 'receive', 'channel': 'chat', 'server': 'localhost:9092', 'group': None, 'from': 'start'}
+        parser = parsed_args(['receive', '--channel', 'chat', '--server', 'localhost:9092', '--from', 'start'])
+        self.assertIsInstance(parser, dict)
+        self.assertEqual(parser, test_patch.return_value)
+
+
+    def test_get_input_func(self):
+        with patch('builtins.input', return_value='y'):
+            assert get_input() == 'y'
+        with patch('builtins.input', return_value='q'):
+            self.assertRaises(SystemExit)
+        with self.assertRaises(SystemExit) as cm:
+            with patch('builtins.input', return_value='q'):
+                get_input()
+        self.assertEqual(cm.exception.code, 0)
+
+
+    def test_send_messages(self):
+        """testing sending of message via kafka"""
+        args = {'command': 'send', 'channel': 'chat', 'server': 'localhost:9092', 'group': None, 'from': 'start'}
+
+        with patch('builtins.input', return_value='hello world david'):
+            self.assertEqual(send_messages(args), True)
+        with patch('builtins.input', return_value='q'):
+            self.assertRaises(SystemExit)
+
+
+    def test_read_messages(self):
+        args = {'command': 'receive', 'channel': 'chat', 'server': 'localhost:9092', 'group': None, 'from': 'start'}
+        self.assertEqual(read_messages(args), True)
 
 
 
