ca0ef16116ca084173b23d44a5f582f2459ffde2
davidkaggulire <dkaggulire@gmail.com>
2021-10-26 12:21:57 +0300

    modify run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import argparse
-from chatapp import parse_args, send_messages, read_messages
+import sys
+from chatapp import parsed_args, send_messages, read_messages
 
 if __name__ == '__main__':
-    args = parse_args()
+    args = parsed_args(sys.argv[1:])
+    print(args)
     if args['command'] == "send":
         send_messages(args)
     elif args['command'] == "receive":
